9b30c9231e5688640228644df49b5bb866061d2b
mari-no <marin.nosova@gmail.com>
2026-08-26 16:59:30 -0500

    comments, fixes, data transformation

--- weather_scraping.py
+++ weather_scraping.py
@@ -7,51 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-# options = webdriver.ChromeOptions()
-
-# options.add_argument(
-#     "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-#     "AppleWebKit/537.36 (KHTML, like Gecko) "
-#     "Chrome/151.0.0.0 Safari/537.36"
-# )
-
-# driver = webdriver.Chrome(
-#     service=ChromeService(ChromeDriverManager().install()),
-#     options=options
-# )
-
-# capitals_climate_URL = "https://www.timeanddate.com/weather/?low=c"
-
-
-# try:
-#     driver.get(capitals_climate_URL)
-   
-#     capitals_table = driver.find_element(By.CSS_SELECTOR, '.zebra.fw.tb-theme')
-   
-    
-#     cities = capitals_table.find_elements(By.CSS_SELECTOR, 'td a[href^="/weather/"]')
-#     capitals_list = []
- 
-#     for city in cities:
-#         href = city.get_attribute("href")
-#         capitals_list.append({"City":city.text,
-#                                "Link": href })
-
-#     print(capitals_list)
-
-
-
-
-
-  
-# except Exception as e:
-#     print("couldn't get the web page")
-#     print(f"Exception: {type(e).__name__} {e}")
-
-# finally:
-#     driver.quit()
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
 options.page_load_strategy = "eager"
@@ -60,17 +15,17 @@ driver = webdriver.Chrome(options=options)
 
 try:
     driver.set_page_load_timeout(30)
-
+#load capitals page
     driver.get("https://www.timeanddate.com/weather/?low=c")
 
     print("Page loaded!")
     print(driver.title)
-
+#load all the capitals 
     cities = driver.find_elements(
     By.XPATH,
     '//table[contains(@class, "zebra")]//td/a[@href]'
 )
-
+#create a list of all capitals
     capitals_list = []
 
     for city in cities:
@@ -81,7 +36,7 @@ try:
     
 
     print(capitals_list)
-
+#create a function that scrapes data for each capital for current month
     def scrape_monttly_weater_by_city(driver, city, url):
         climate_monthly = []
         driver.get(url)
@@ -101,7 +56,8 @@ try:
                  By.XPATH,
             ".//span[contains(text(), 'High Temp:')]/.."
         ).text.replace("High Temp: ", "").strip()
-         
+         #search inside the current element and all of its descendants for the text,
+         # and after that text is found go to the parent element of this span
             low_temp = month.find_element(
             By.XPATH,
             ".//span[contains(text(), 'Low Temp:')]/.."
@@ -114,7 +70,7 @@ try:
             By.XPATH,
             ".//span[contains(text(), 'Precipitation:')]/.."
         ).text.replace("Precipitation: ", "").strip()
-
+ #add scarped data to the list
             climate_monthly.append({
                 "City": city,
                 "Month": month_name,
@@ -129,14 +85,18 @@ try:
     capitals_climate_monthly = []
     for city in capitals_list:
         city_name = city["City"]
+        #create URL for each capital based on info we scraped and adding /climate
         city_climate_URL = city["Link"]+"/climate"
+        #call created function with the url created for each city
         climate_monthly =  scrape_monttly_weater_by_city(driver, city_name, city_climate_URL)
+        #take all the items from climate_monthly and add them to capitals_climate_monthly
         capitals_climate_monthly.extend(climate_monthly)
-        print(capitals_climate_monthly[0:50])
+        #print(capitals_climate_monthly[0:50])
 
-
+#create a df from this list of dict
 
     climate_df = pd.DataFrame(capitals_climate_monthly)
+#write df to csv
     climate_df.to_csv("climate_data.csv", index=False)
 finally:
     driver.quit()
